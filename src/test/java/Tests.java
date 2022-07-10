30e787a317eb783d8c6d0d70a02305b3b0401d73
user <vardankhublaryan2@gmail.com>
2022-07-11 00:19:57 +0400

    ,ll

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,17 +1,19 @@
 
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.*;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 import io.restassured.response.ValidatableResponse;
@@ -31,17 +33,24 @@ public class Tests {
         get("https://gorest.co.in/public/v2/users")
                 .then()
                 .statusCode(200)
-                .body("[0].id", equalTo(4108))
+                .body("[0].id", equalTo(4102))
                 .body("[0].email", equalTo("mrs_shakti_sharma@wintheiser-kerluke.org"));
     }
+
+    @Test
+    public void generic(){
+        List<Map<String,Object>> users = get("https://gorest.co.in/public/v2/users").as(new TypeRef<List<Map<String, Object>>>() {});
+        assertThat(users.get(0).get("id"),equalTo(4102));
+        assertThat(users.get(1).get("email"),equalTo("aarya_dutta@haag.io"));
+        assertThat(users.get(2).get("gender"),equalTo("male"));
+    }
+
 
     @Test
     public void post() {
         String jsonString = "{\"id\":\"41055156139\",\"name\":\"Vardan\",\"email\":\"vasrsv@gmail.com\"}";
 
         RestAssured.baseURI = "https://gorest.co.in/public/v2/users";
-
-
         requestSpecification = RestAssured
                 .given()
                 .contentType(ContentType.JSON)
