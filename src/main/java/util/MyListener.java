f0100d26acf91e0b1c3e3a7146691103d5ab8b33
user <vardankhublaryan2@gmail.com>
2022-07-29 10:30:07 +0400

    ,ll

--- src/main/java/util/MyListener.java
+++ src/main/java/util/MyListener.java
@@ -4,31 +4,33 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import static util.MyLog4j.*;
+
 
 public class MyListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
-        System.out.println("Start: " + result.getName());
+        info("Start: " + result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("Success of test cases and its details are : " + result.getName());
+        info("Success of test cases and its details are : " + result.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("Failure of test cases and its details are : " + result.getName());
+        error("Failure of test cases and its details are : " + result.getName());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println("Skip of test cases and its details are : " + result.getName());
+        info("Skip of test cases and its details are : " + result.getName());
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        System.out.println("Failure of test cases and its details are : " + result.getName());
+        error("Failure of test cases and its details are : " + result.getName());
     }
 
     @Override
